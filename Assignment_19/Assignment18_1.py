a347db8b5d161d820d74419625116b64133f39a3
Rahul Jadhav <jadhavrahul1824@gmail.com>
2026-05-02 23:49:56 +0530

    Add square function and main execution flow

---
+++ Assignment_19/Assignment18_1.py
@@ -0,0 +1,10 @@
+Square = lambda iNo : iNo**2
+
+def main():
+
+    Value = int(input("Enter a Number : "))
+    Ret = Square(Value)
+    print("Square of Number : ", Ret)
+
+if __name__ == "__main__":
+    main()
