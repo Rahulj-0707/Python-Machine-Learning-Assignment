96c02e8c10efc46210e995a8282d504ed659a7c0
Rahul Jadhav <jadhavrahul1824@gmail.com>
2026-05-03 00:22:32 +0530

    Create Assignment48_1.py

---
+++ Assignment_48/Assignment48_1.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def main():
+
+    Arr = np.array([6, 7, 8, 9, 10, 11, 12])
+
+    Mean = Arr.mean()
+
+    print("Mean of the Dataset is : ",Mean)
+     
+if __name__ == "__main__":
+    main()
+
