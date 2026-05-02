173579a6cf5507f475e01779368e8867ef581304
Rahul Jadhav <jadhavrahul1824@gmail.com>
2026-05-02 09:03:25 +0530

    Create Assignment14_1.py

---
+++ Assignment_14/Assignment14_1.py
@@ -0,0 +1,9 @@
+Square = lambda No : (No**2)
+
+def main():
+    Value = int(input("Enter a number : "))
+    Ret = Square(Value)
+    print("Square of a number is : ",Ret)
+
+if __name__ == "__main__":
+    main()
